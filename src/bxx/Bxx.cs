24194e235fa061b8b3535503da9feb07262ddc41
midbel <brun.nicolas21@gmail.com>
2020-03-30 08:11:59 +0200

    extension methods

--- src/bxx/Bxx.cs
+++ src/bxx/Bxx.cs
@@ -15,4 +15,36 @@ namespace bxx
         string Decode(string str);
         string Decode(byte[] str);
     }
+
+    namespace bxx.strings.extensions
+    {
+        public static class ExtendString
+        {
+            public static string ToBase64Std(this String str)
+            {
+                return Base64.Std.Encode(str);
+            }
+
+            public static string ToBase64Url(this String str)
+            {
+                return Base64.Url.Encode(str);
+            }
+
+            public static string ToBase32Std(this String str)
+            {
+                return Base32.Std.Encode(str);
+            }
+
+            public static string ToBase32Hex(this String str)
+            {
+                return Base32.Hex.Encode(str);
+            }
+
+            public static string ToBase16(this String str)
+            {
+                return Base16.Std.Encode(str);
+            }
+        }
+    }
+
 }
